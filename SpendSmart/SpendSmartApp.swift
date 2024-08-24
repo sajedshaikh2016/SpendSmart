e33900e73ddcd9879304531c640ce1f7fe874c98
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-08-24 12:00:45 +0530

    Initial Commit

---
+++ SpendSmart/SpendSmartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendSmartApp.swift
+//  SpendSmart
+//
+//  Created by Sajed Shaikh on 24/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SpendSmartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
